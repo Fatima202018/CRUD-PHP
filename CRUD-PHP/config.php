82ed8c28da4bfc8deb2500db92fdce8d8a84ec99
Fatima202018 <73596272+Fatima202018@users.noreply.github.com>
2022-08-29 12:46:42 -0600

    Add files via upload

---
+++ CRUD-PHP/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'crud',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
